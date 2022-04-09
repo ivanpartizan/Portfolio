4a07bb312a2b3d4303928b33161ea0c53d59f630
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-04-09 23:33:36 +0200

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-function boxShadow() {
-  const scrollHeader = document.getElementById("header");
-
-  if (this.scrollY >= 80) scrollHeader.classList.add("headerActive");
-  else scrollHeader.classList.remove("headerActive");
-}
-
-window.addEventListener("scroll", boxShadow);
